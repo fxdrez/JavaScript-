459d85becc50f7a9fcc4b71f2338849f1c73d0ea
fxdrez <nirmalgajurel00@gmail.com>
2025-01-08 14:17:04 +0545

    consolelog practice

---
+++ practice1.cjs
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  const output = document.getElementById("output");
+  output.textContent = console.log("I love you");
+});
